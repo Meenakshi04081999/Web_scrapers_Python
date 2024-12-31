f9789220fe56683d77280c1a6b130253db18c018
Meenakshi Rajendran <149500334+Meenakshi04081999@users.noreply.github.com>
2024-12-31 01:39:31 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+url = "https://en.wikipedia.org/wiki/Main_Page"
+
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text,"html.parser")
+
+#print(soup)
+
+headers = []
+
+for header in soup.find_all(["h1","h2","h3","h4","h5","h6"]):
+    text = header.text.strip()
+    level = header.name
+    headers.append((text,level))
+
+#print(headers)
+
+df = pd.DataFrame(headers,columns=["Text","Level"])
+print(df)
